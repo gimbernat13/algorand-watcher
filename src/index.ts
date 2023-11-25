9908d6fa49e00689690e5904c369cc87464af780
Diego <gimbernat13@gmail.com>
2023-11-25 13:16:33 -0500

    added CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import watcherRoutes from "./routes/watcher.routes";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
-
+// TODO: Change to env variable
+const allowedOrigins = ["http://localhost:3000"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+app.use(cors(options));
 app.use(watcherRoutes);
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
