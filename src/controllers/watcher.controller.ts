b123f2639fe10fe1e20bff3e71b9d945d5aa0eef
Diego <gimbernat13@gmail.com>
2023-11-25 01:58:12 -0500

    fetch account

--- src/controllers/watcher.controller.ts
+++ src/controllers/watcher.controller.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
 import * as dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
-const accounts: string[] = [];
+const accounts: string[] = [
+  "ZW3ISEHZUHPO7OZGMKLKIIMKVICOUDRCERI454I3DB2BH52HGLSO67W754",
+];
+
+export async function fetchAccountData(address: string) {
+  console.log("fetching acc data ");
+  try {
+    const url = `https://mainnet-api.algonode.cloud/v2/accounts/${address}`;
+    const response = await axios.get(url);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null; // or handle the error as you see fit
+  }
+}
 
 export const addAccount = async (req: Request, res: Response) => {
   const address = req.params.address;
@@ -15,6 +31,7 @@ export const addAccount = async (req: Request, res: Response) => {
     console.log("error", error);
   }
 };
+
 export const removeAccount = async (req: Request, res: Response) => {
   try {
   } catch (error) {}
