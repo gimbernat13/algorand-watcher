7ab397cce4912a26a2345926c4a77f4afde3e5ba
Diego <gimbernat13@gmail.com>
2023-11-25 11:14:02 -0500

    Minimize state updates

--- src/controllers/watcher.controller.ts
+++ src/controllers/watcher.controller.ts
@@ -7,39 +7,48 @@ dotenv.config();
 const accounts: string[] = [
   "OM6MRCKILRNNBYSSM3NHOQVRCX4EKF3YMA4EKHIGYWAV553Y57ZO2B7EFM",
   "IVBHJFHZWPXRX2EA7AH7Y4UTTBO2AK73XI65OIXDEAEN7VO2IHWXKOKOVM",
+  "KWNBRP4E6X7POFIPCDNX5NZBY2YHW4RRH67RBWRCII5BZVZ5NLTGCBANUU",
+  "7O3IVAXXX645ZDKBOJIRXW7ULW4B77KK4B5KGVRIIYR3CTK2U5KRLLXWFQ",
+  "ZW3ISEHZUHPO7OZGMKLKIIMKVICOUDRCERI454I3DB2BH52HGLSO67W754",
 ];
-interface AccountBalances {
+interface accountState {
   [key: string]: number | null;
 }
 
-const accountBalances: AccountBalances = {};
+let accountState: accountState = {};
 async function checkAccountStates() {
   console.log("🚧 Checking Account States");
+  const updatedAccState = { ...accountState };
+  let stateHasChanged = false;
   for (const account of accounts) {
     try {
-      const accountData = await fetchAccountData(account);
-      const currentBalance = accountData ? accountData.amount : null;
-      accountBalances[account] = currentBalance;
-
+      const accountBalance = await fetchAccountData(account);
+      const currentBalance = accountBalance ? accountBalance : null;
+      updatedAccState[account] = currentBalance;
       if (
-        accountBalances[account] !== undefined &&
-        accountBalances[account] !== currentBalance
+        accountState[account] !== undefined &&
+        accountState[account] !== currentBalance
       ) {
-        console.log(
-          `Balance changed for account ${account}. Previous: ${accountBalances[account]}, Current: ${currentBalance}`
+        stateHasChanged = true;
+        console.table(
+          `${account}. Previous: ${accountState[account]}, Current: ${currentBalance}`
         );
       }
     } catch (error) {
       console.error(`Error fetching data for account ${account}:`, error);
     }
   }
+  if (stateHasChanged) {
+    console.log("Updating State ", accountState);
+    accountState = updatedAccState;
+  }
+
+  console.log("Accounts State Updated", accountState);
 }
 
 setInterval(checkAccountStates, 6000);
 
 export async function fetchAccountData(address: string) {
-  console.log("Accounts State ", accountBalances);
-  console.log("🚧 Fetching: ", address);
   try {
     const url = `https://mainnet-api.algonode.cloud/v2/accounts/${address}`;
     const response = await axios.get(url);
